80c6fbd170ec25aa1f1c43f531ac69043f8f7c10
Luiz Otávio Jirkowsky e Silva <71289845+Wolxf2@users.noreply.github.com>
2023-06-29 22:14:59 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,4 +6,5 @@ virtualenv -ppython3 venv
 
 source venv/bin/activate
 
-pip install -r Flask-AWS-Example/requirements.txt
+#pip install -r Flask-AWS-Example/requirements.txt
+pip install -r requirements.txt
